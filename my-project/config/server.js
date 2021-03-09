143f95dd6569393d0d846a2127a6d0c720c7b37a
NoorShinwari <nanoshin746@gmail.com>
2021-03-09 11:01:36 +0100

    first commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ba7364bda6c4e76177164d847ac1b62'),
+    },
+  },
+});
